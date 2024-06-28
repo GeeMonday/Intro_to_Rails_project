d1363ac95fc52d72e51555d58f86b6955554e465
GeeMonday <gmonday@academic.rrc.ca>
2024-06-28 12:58:17 -0500

    added faker

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+  has_many :applications
+  has_many :favorites
+  has_many :favorited_by_users, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+    has_many :applications
+    has_many :favorites
+    has_many :favorite_pets, through: :favorites, source: :pet
+  end
