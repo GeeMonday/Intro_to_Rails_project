b23466ef8d75a46c04105452a08eaf7f4f42becb
GeeMonday <gmonday@academic.rrc.ca>
2024-06-28 19:02:46 -0500

    updated home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
   def index
+    @featured_pets = Pet.limit(5).order("RANDOM()")
+    @shelters = Shelter.limit(3)
+    @users = User.limit(3)
   end
 end
