6cf62bd45086899f86b257800756f45959e02f2d
GeeMonday <gmonday@academic.rrc.ca>
2024-06-29 01:24:39 -0500

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
-  resources :pets, only: [:index, :show]
+  resources :dogs, only: [:index, :show]
   resources :shelters, only: [:index, :show]
   resources :applications, only: [:index, :show]
   resources :favorites, only: [:index, :show]
