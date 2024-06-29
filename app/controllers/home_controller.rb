fc26ca0d42e867309fc41f2dcdf2dcd4fe04d126
GeeMonday <gmonday@academic.rrc.ca>
2024-06-29 01:05:49 -0500

    upadted dog view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 # app/controllers/home_controller.rb
 class HomeController < ApplicationController
   def index
-    @featured_pets = Pet.limit(5).order("RANDOM()")
-    @shelters = Shelter.limit(3)
-    @users = User.limit(3)
+    @dogs = Dog.limit(10)
+    @shelters = Shelter.all
+    @users = User.all
   end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,6 @@ class Dog < ApplicationRecord
   has_many :applications
   has_many :favorites
   has_many :favorited_by_users, through: :favorites, source: :user
+
+  validates :name, :breed, :age, :description, :photo_url, presence: true
 end
