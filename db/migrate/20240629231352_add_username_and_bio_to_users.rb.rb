c3077341f6e4025e12d4c7f795bba97d7d67c201
GeeMonday <gmonday@academic.rrc.ca>
2024-06-29 18:36:32 -0500

    fixed migration issues

--- db/migrate/20240629231352_add_username_and_bio_to_users.rb.rb
+++ db/migrate/20240629231352_add_username_and_bio_to_users.rb.rb
@@ -1,4 +1,4 @@
-class RenameUsernameAndAddBioToUsers < ActiveRecord::Migration[7.1]
+class AddUsernameAndBioToUsers < ActiveRecord::Migration[7.1]
   def change
     rename_column :users, :username, :new_username
     add_column :users, :bio, :text
