c2ad0ff8031fa3ed868fc8bb69aa21d980b03d8e
GeeMonday <gmonday@academic.rrc.ca>
2024-07-02 11:10:48 -0500

    added profile_photo to registration controller and User model

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters
 
   protected
 
@@ -10,5 +11,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:new_username, :bio])
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:new_username, :bio, :profile_photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:new_username, :bio, :profile_photo])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,32 @@
 class User < ApplicationRecord
-  # Assuming you renamed it to `new_username`
-  validates :new_username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_one_attached :profile_photo
+
+  # Validations
+  validates :new_username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :bio, presence: true, length: { maximum: 500 }
+  validate :correct_profile_photo_type
+  validate :correct_profile_photo_size
 
+  # Devise modules
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  
-  # If you're using `has_secure_password` with Devise
-  has_secure_password validations: false
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
+  # Associations
   has_many :applications
   has_many :favorites
-  has_many :favorite_pets, through: :favorites, source: :dog
-end
+  has_many :favorite_pets, through: :favorites, source: :pet
 
+  private
+
+  def correct_profile_photo_type
+    if profile_photo.attached? && !profile_photo.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:profile_photo, 'must be a JPEG or PNG')
+    end
+  end
+
+  def correct_profile_photo_size
+    if profile_photo.attached? && profile_photo.byte_size > 5.megabytes
+      errors.add(:profile_photo, 'size needs to be less than 5MB')
+    end
+  end
+end
